65d24cd8b173267483a092c4d66c730cbdde3d2f
ワルンポ-ンさん <tankubopa77@gmail.com>
2023-11-09 18:10:01 +0700

    Initial Commit

---
+++ NumberPuzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NumberPuzzle
+//
+//  Created by warunporn intarachana on 9/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NumberPuzzle/NumberPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberPuzzleApp.swift
+//  NumberPuzzle
+//
+//  Created by warunporn intarachana on 9/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct NumberPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
